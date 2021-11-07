39dacbcfbb3968f4d09c8766f2b4c1019cf006d3
Jonas Nockert <jonasnockert@gmail.com>
2021-11-07 03:27:07 +0100

    Generate grid of cars

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,8 @@
 import {
-  BufferGeometry,
-  CameraHelper,
   Color,
   DirectionalLight,
-  DoubleSide,
-  Euler,
-  Line,
-  LineLoop,
-  LineBasicMaterial,
-  Math as ThreeMath,
-  Matrix4,
-  Mesh,
-  MeshBasicMaterial,
-  MeshPhongMaterial,
   PerspectiveCamera,
-  Plane,
-  PlaneGeometry,
-  Ray,
   Scene,
-  Shape,
-  ShapeGeometry,
-  Triangle,
-  Vector3,
   WebGLRenderer,
 } from "three";
 import { proceduralCar } from "./car";
@@ -36,7 +17,7 @@ let app = {
   scene: null,
   renderer: null,
   camera: null,
-  car: null,
+  cars: [],
 };
 
 const init = () => {
@@ -48,9 +29,9 @@ const init = () => {
     nearDistance,
     farDistance,
   );
-  app.camera.position.z = 0;
+  app.camera.position.z = 15;
   app.camera.position.y = 2;
-  app.camera.position.x = -7;
+  app.camera.position.x = 0;
   app.camera.lookAt(0, 0, 0);
   app.renderer = new WebGLRenderer();
   app.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -62,31 +43,24 @@ const init = () => {
   light.position.set(-1, 2, 4);
   app.scene.add(light);
 
-  app.car = proceduralCar();
-  app.scene.add(app.car);
+  for (let x = -1; x < 2; x++) {
+    for (let y = -1; y < 2; y++) {
+      const car = proceduralCar();
+      car.position.x = x * 5;
+      car.position.y = y * 3;
+      app.cars.push(car);
+      app.scene.add(car);
+    }
+  }
 };
 
 const animate = () => {
   requestAnimationFrame(animate);
-  app.car.rotation.y += 0.01;
+  for (const c of app.cars) {
+    c.rotation.y += 0.01;
+  }
 
   app.renderer.render(app.scene, app.camera);
-};
-
-const createMaterial = () => {
-  // const material = new MeshPhongMaterial({
-  //   side: DoubleSide,
-  // });
-  const material = new MeshPhongMaterial({
-    color: 0x44aa88,
-    side: DoubleSide,
-  });
-
-  // const hue = Math.random();
-  // const saturation = 1;
-  // const luminance = 0.5;
-  // material.color.setHSL(hue, saturation, luminance);
-  return material;
 };
 
 init();
